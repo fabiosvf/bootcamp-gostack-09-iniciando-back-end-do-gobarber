a4925513c02a1d9baa8be4bb357fc59a85a4b966
Fabio Silva Ferreira <fabiosvf@gmail.com>
2019-12-01 18:10:29 -0300

    Módulo 'Continuando API do GoBarber', Vídeo 'Avatar do usuário'

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,3 +1,5 @@
+// https://www.md5online.org/
+// The MD5 hash for gobarberrocketseatnode2 is : f29618255c309de4469993cce24286ea
 export default {
   secret: 'f29618255c309de4469993cce24286ea',
   expiresIn: '7d',
