2c3c6a9d543284209c49c06a80f4f228190b44be
Fabio Silva Ferreira <fabiosvf@gmail.com>
2019-12-03 00:13:46 -0300

    Módulo 'Continuando API do GoBarber', Vídeo 'Marcar notificações como lidas'

--- src/routes.js
+++ src/routes.js
@@ -21,11 +21,17 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
