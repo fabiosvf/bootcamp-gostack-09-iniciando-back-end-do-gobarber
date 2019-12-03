675c8195df397d11218e6b6048ac06af811f94b8
Fabio Silva Ferreira <fabiosvf@gmail.com>
2019-12-03 01:01:32 -0300

    Módulo 'Continuando API do GoBarber', Vídeo 'Configurando Nodemailer'

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '80b8d64521ce9b',
+    pass: '010ed9c0cc5e9c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
